c8dd196309e40b62e87bca79159d6519af902f33
purushotham0113 <229x1a33b3@gmail.com>
2025-12-31 20:17:27 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5000"]
+
+}))
 app.use(express.json())
 
 async function connectDB() {
